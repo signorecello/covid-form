4a6ce62fdeb9ef216507928bde1b5b426bf10fed
José Pedro Sousa <jpedrosous@gmail.com>
2020-03-31 16:33:29 +0100

    enabled print before submit

--- src/public/javascripts/addField.js
+++ src/public/javascripts/addField.js
@@ -38,15 +38,44 @@ function showProcedure() {
 
 $(document).ready(function(){
     $("#btnSubmit").click(function(event) {
-
         var form = $("#main-form")
 
+        event.preventDefault()
         if (form[0].checkValidity() === false) {
-            event.preventDefault()
             event.stopPropagation()
+        } else {
+
+            var formArray = $("form").serializeArray();
+            printJS({
+                printable: "main-form",
+                type: "html",
+                onPrintDialogClose: () => {
+                    $.post(`/submit`, formArray)
+                    window.location.replace("/form")
+                }
+            })
+
+
         }
-        
+
+
         form.addClass('was-validated');
         
     });
+
+    $("#Identification").keyup(function(event) {
+        $.post(`/validate/identification`, 
+        {
+            Identification: event.target.value
+        }, function(data, status) {
+            let input = document.getElementById("Identification");
+            if (data) {
+                input.classList.remove("is-invalid");
+                input.classList.add("is-valid");
+            } else {
+                input.classList.remove("is-valid");
+                input.classList.add("is-invalid");
+            }
+        })
+    })
 });
